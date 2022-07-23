b80033dd9974bb37de6773615aa800c0a00a2bd1
Dušan Osmokrović <60246031+d855@users.noreply.github.com>
2022-07-23 11:58:22 +0200

    routes, model and controller

---
+++ resources/views/images/index.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport"
+		      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>Image uploader</title>
+		<script src="https://cdn.tailwindcss.com"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+	<body>
+		<div class="max-w-lg mx-auto mt-24">
+			<h1 class="text-4xl font-bold text-center">Image uploader</h1>
+		</div>
+	</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\ImageController;
+    use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::get('/', [ImageController::class, 'index']);
+
+    Route::get('/images', [ImageController::class, 'show']);
+
+    Route::post('/upload', [ImageController::class, 'store']);
